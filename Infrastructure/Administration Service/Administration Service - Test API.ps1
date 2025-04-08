8d846043134b1d1decfbdfedcd891b3dd78a312d
Dustin Estes <85371705+dustinestes@users.noreply.github.com>
2025-04-08 06:24:58 -0500

    fixed formatting

--- Infrastructure/Administration Service/Administration Service - Test API.ps1
+++ Infrastructure/Administration Service/Administration Service - Test API.ps1
@@ -10,20 +10,21 @@ for ($i = 1; $i -lt 1000; $i++) {
 
   # Execute Test
   $Measure = Measure-Command {
-      $Odata_Root   = "https://$($SMSProvider)/AdminService/wmi/"
-      $Odata_Resource = "SMS_Site"
       $Result = Invoke-RestMethod -Uri "$($Odata_Root + $Odata_Resource)" -Method Get -ContentType "application/json" -UseDefaultCredentials
+    $Odata_Root   = "https://$($SMSProvider)/AdminService/wmi/"
+    $Odata_Resource = "SMS_Site"
+    Invoke-RestMethod -Uri "$($Odata_Root + $Odata_Resource)" -Method Get -ContentType "application/json" -UseDefaultCredentials
   }
 
   # Construct Object
   $Temp_Object = [PSCustomObject]@{
-      Job             = $i
-      StartExecute    = $Start
-      Requestor       = $($env:COMPUTERNAME)
-      WebAPIURI       = "$($Odata_Root + $Odata_Resource)"
-      DatabaseServer  = "0003wp-dbag-04v.flightsafety.com"
-      ObjectsReturned = $($Result.Value.Count)
-      ExecutionTime   = "$($Measure.Days):$($Measure.Hours):$($Measure.Minutes):$($Measure.Seconds):$($Measure.Milliseconds)"
+    Job             = $i
+    StartExecute    = $Start
+    Requestor       = $($env:COMPUTERNAME)
+    WebAPIURI       = "$($Odata_Root + $Odata_Resource)"
+    DatabaseServer  = "0003wp-dbag-04v.flightsafety.com"
+    ObjectsReturned = $($Result.Value.Count)
+    ExecutionTime   = "$($Measure.Days):$($Measure.Hours):$($Measure.Minutes):$($Measure.Seconds):$($Measure.Milliseconds)"
   }
 
   # Append Dataset
@@ -35,5 +36,5 @@ for ($i = 1; $i -lt 1000; $i++) {
   Write-Host "  Completed: $((Get-Date).ToString("yyyy-MM-dd HH:mm:ssz"))"
 
   # Sleep Timer
-    Start-Sleep -Seconds 60
+  Start-Sleep -Seconds 60
 }
