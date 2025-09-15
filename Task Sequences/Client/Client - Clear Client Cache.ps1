b5c07e19007c0dfd27b89dfb40b9504b21f0c133
Dustin Estes <85371705+dustinestes@users.noreply.github.com>
2025-09-15 10:15:40 -0500

    converted to new template

--- Task Sequences/Client/Client - Clear Client Cache.ps1
+++ Task Sequences/Client/Client - Clear Client Cache.ps1
@@ -3,114 +3,467 @@
 #--------------------------------------------------------------------------------------------
 
 # param (
-#     [string]$ParameterName
+#   [string]$SiteCode,                  # 'ABC'
+#   [string]$SMSProvider,               # '[ServerFQDN]'
+#   [string]$ParamName                  # '[ExampleInputValues]'
 # )
 
 #--------------------------------------------------------------------------------------------
-
-
-#--------------------------------------------------------------------------------------------
-# Write Output Header Info to SMSTS.log
-#--------------------------------------------------------------------------------------------
-    Write-Host "------------------------------------------------------------------------------"
-    Write-Host "  MECM Toolkit - Task Sequences - Certificate - Enroll in Certificate"
-    Write-Host "------------------------------------------------------------------------------"
-    Write-Host "    Author:    Dustin Estes"
-    Write-Host "    Company:   VividRock"
-    Write-Host "    Date:      December 23, 2019"
-    Write-Host "    Copyright: VividRock LLC - All Rights Reserved"
-    Write-Host "    Purpose:   This script will attempt to enroll in the specified certificate"
-    Write-Host "               template using the server specified."
-    Write-Host "------------------------------------------------------------------------------"
-    Write-Host ""
-#--------------------------------------------------------------------------------------------
-
+# Start-Transcript -Path "C:\VividRock\MECM Toolkit\Logs\Task Sequence\Client - Clear Client Cache.log" -ErrorAction SilentlyContinue
 
 #--------------------------------------------------------------------------------------------
-# Set Variables
+# Header
 #--------------------------------------------------------------------------------------------
-    # Create Com Object
-        $MECMObject = New-Object -ComObject "UIResource.UIResourceMgr"
+#Region Header
 
-    # Set Variables
-        $ElementCounter_Success = 0
-        $ElementCounter_Error = 0
-        $ElementCounter_Total = $MECMObject.GetCacheInfo().GetCacheElements().Count
+  Write-Host "------------------------------------------------------------------------------"
+  Write-Host "  MECM Toolkit - Task Sequence - Client - Clear Client Cache"
+  Write-Host "------------------------------------------------------------------------------"
+  Write-Host "    Author:     Dustin Estes"
+  Write-Host "    Company:    VividRock"
+  Write-Host "    Date:       December 23, 2019"
+  Write-Host "    Copyright:  VividRock LLC - All Rights Reserved"
+  Write-Host "    Purpose:    This script will attempt to clear the client cache."
+  Write-Host "    Links:      None"
+  Write-Host "    Template:   1.1"
+  Write-Host "------------------------------------------------------------------------------"
+  Write-Host ""
 
+<#
+  To Do:
+    - Item
+    - Item
+#>
+
+#EndRegion Header
 #--------------------------------------------------------------------------------------------
 
 
 #--------------------------------------------------------------------------------------------
-# Main Execution
+# Variables
+#   Error Range: 1200 - 1299
 #--------------------------------------------------------------------------------------------
-    # Get Cache Elements
-        Write-Host "      - Get Cache Elements"
+#Region Variables
 
-        try {
-            $MECMCache_Elements = $MECMObject.GetCacheInfo().GetCacheElements()
+  Write-Host "  Variables"
 
-            Write-Host "          Count: $($MECMCache_Elements.Count)"
+  # Parameters
+    $Param_SiteCode         = $SiteCode
+    $Param_SMSProvider      = $SMSProvider
+    $Param_ParamName        = $ParamName
+
+  # Metadata
+    $Meta_Script_Start_DateTime     = Get-Date
+    $Meta_Script_Complete_DateTime  = $null
+    $Meta_Script_Complete_TimeSpan  = $null
+    $Meta_Script_Execution_User     = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $Meta_Script_Result = $false,"Failure"
+
+  # Preferences
+    $ErrorActionPreference        = "Stop"
+    $CMPSSuppressFastNotUsedCheck = $true
+
+  # Names
+
+  # Paths
+    # $Path_AdminService_ODataRoute = "https://$($Param_SMSProvider)/AdminService/v1.0/"
+    # $Path_AdminService_WMIRoute   = "https://$($Param_SMSProvider)/AdminService/wmi/"
+
+  # Files
+
+  # Hashtables
+
+  # Arrays
+
+  # Registry
+
+  # WMI
+
+  # Datasets
+
+  # Temporary
+	$Temp_Counter_Success 	= 0
+	$Temp_Counter_Error 	= 0
+	$Temp_Counter_Total 	= 0
+
+  # Output to Log
+    Write-Host "    - Parameters"
+    foreach ($Item in $PSBoundParameters.GetEnumerator()) {
+      Write-Host "        $($Item.Key.PadRight(($PSBoundParameters.Keys | Measure-Object -Property Length -Maximum).Maximum + 1)): $($Item.Value)"
+    }
+
+  Write-Host "    - Complete"
+  Write-Host ""
+
+#EndRegion Variables
+#--------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------
+# Functions
+#   Error Range: 1300 - 1399
+#--------------------------------------------------------------------------------------------
+#Region Functions
+
+  Write-Host "  Functions"
+
+  # Write Error Codes
+    Write-Host "    - Write-vr_ErrorCode"
+    function Write-vr_ErrorCode ($Code,$Exit,$Object) {
+      # Code: XXXX   4-digit code to identify where in script the operation failed
+      # Exit: Boolean option to define if  exits or not
+      # Object: The error object created when the script encounters an error ($Error[0], $PSItem, etc.)
+
+      begin {
+
+      }
+
+      process {
+        Write-Host "        Error: $($Object.Exception.ErrorId)"
+        Write-Host "        Command Name: $($Object.CategoryInfo.Activity)"
+        Write-Host "        Message: $($Object.Exception.Message)"
+        Write-Host "        Line/Position: $($Object.Exception.Line)/$($Object.Exception.Offset)"
+      }
+
+      end {
+        switch ($Exit) {
+          $true {
+            Write-Host "        Exit: $($Code)"
+            Stop-Transcript
+            Exit $Code
+          }
+          $false {
+            Write-Host "        Continue Processing..."
+          }
+          Default {
+            Write-Host "        Unknown Exit option in Write-vr_ErrorCode parameter"
+          }
         }
-        catch {
-            Write-Host "          Error: Failed to Get Cache Elements"
-            Write-Host "          Command Name: "$Error[0].Exception.CommandName
-            Write-Host "          Message: "$Error[0].Exception.Message
-            Exit 1401
-        }
+      }
+    }
 
-    # Delete Cache Elements
-        Write-Host "      - Delete Cache Elements"
+  Write-Host "    - Complete"
+  Write-Host ""
 
-        try {
-            foreach ($Item in $MECMCache_Elements) {
-                Write-Host "          Content ID: $($Item.ContentId)"
-                Write-Host "          Version: $($Item.ContentVersion)"
-                Write-Host "          Location: $($Item.Location)"
-                Write-Host "          Last Reference Time: $($Item.LastReferenceTime)"
-                Write-Host "          Size: $([math]::Round($Item.ContentSize / 1kb, 2)) MB"
+#EndRegion Functions
+#--------------------------------------------------------------------------------------------
 
-                $MECMObject.GetCacheInfo().DeleteCacheElement([string]$($Item.CacheElementID))
-
-                Write-Host "          Status: Delete Attempted"
-                Write-Host ""
-            }
-        }
-        catch {
-            $ElementCounter_Error ++
-            Write-Host "          Error: Failed to Delete Cache Element"
-            Write-Host "          Command Name: "$Error[0].Exception.CommandName
-            Write-Host "          Message: "$Error[0].Exception.Message
-            # Exit 1402
-        }
-
-    # Output Status
-        Write-Host "      - Output Status"
-
-        try {
-            $MECMCache_Elements_Validate = $MECMObject.GetCacheInfo().GetCacheElements()
-
-            Write-Host "          Count: $($MECMCache_Elements_Validate.Count)"
-
-            if ($MECMCache_Elements_Validate.Count -ne 0) {
-                Write-Host "          Status: Error. Some Elements Not Deleted"
-            }
-            else {
-                Write-Host "          Status: Success"
-            }
-        }
-        catch {
-            Write-Host "          Error: Failed to Output Status"
-            Write-Host "          Command Name: "$Error[0].Exception.CommandName
-            Write-Host "          Message: "$Error[0].Exception.Message
-            Exit 1401
-        }
 
 #--------------------------------------------------------------------------------------------
-# End of Script
+# Environment
+#   Error Range: 1400 - 1499
 #--------------------------------------------------------------------------------------------
-    Write-Host ""
-    Write-Host "------------------------------------------------------------------------------"
-    Write-Host "  End of Script"
-    Write-Host "------------------------------------------------------------------------------"
+#Region Environment
+
+	Write-Host "  Environment"
+
+	# # Create Client COM Object
+	# 	Write-Host "    - Create Client COM Object"
+
+	# 	try {
+	# 		$Object_MECM_Client = New-Object -ComObject Microsoft.SMS.Client
+	# 		Write-Host "        Status: Success"
+	# 	}
+	# 	catch {
+	# 		Write-vr_ErrorCode -Code 1401 -Exit $true -Object $PSItem
+	# 	}
+
+	# # Create TSEnvironment COM Object
+	# 	Write-Host "    - Create TSEnvironment COM Object"
+
+	# 	try {
+	# 		$Object_MECM_TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment
+	# 		Write-Host "        Status: Success"
+	# 	}
+	# 	catch {
+	# 		Write-vr_ErrorCode -Code 1402 -Exit $true -Object $PSItem
+	# 	}
+
+	# # Connect to MECM Infrastructure
+	# 	Write-Host "    - Connect to MECM Infrastructure"
+
+	# 	try {
+	# 		if (Test-Connection -ComputerName $Param_SMSProvider -Count 2 -Quiet) {
+	# 			# Import the PowerShell Module
+	# 				Write-Host "        Import the PowerShell Module"
+
+	# 				if((Get-Module ConfigurationManager -ErrorAction SilentlyContinue) -in $null,"") {
+	# 					Import-Module -Name "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+	# 					Write-Host "            Status: Success"
+	# 				}
+	# 				else {
+	# 					Write-Host "            Status: Already Imported"
+	# 				}
+
+	# 			# Create the Site Drive
+	# 				Write-Host "        Create the Site Drive"
+
+	# 				if((Get-PSDrive -Name $Param_SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -in $null,"") {
+	# 					New-PSDrive -Name $Param_SiteCode -PSProvider CMSite -Root $Param_SMSProvider | Out-Null
+	# 					Write-Host "            Status: Success"
+	# 				}
+	# 				else {
+	# 					Write-Host "            Status: Already Exists"
+	# 				}
+
+	# 			# Set the Location
+	# 				Write-Host "        Set the Location"
+
+	# 				if ((Get-Location).Path -ne "$($Param_SiteCode):\") {
+	# 					Set-Location "$($Param_SiteCode):\"
+	# 					Write-Host "            Status: Success"
+	# 				}
+	# 				else {
+	# 					Write-Host "            Status: Already Set"
+	# 				}
+	# 		}
+	# 		else {
+	# 			Write-Host "        Status: MECM Server Unreachable"
+	# 			Throw "Status: MECM Server Unreachable"
+	# 		}
+	# 	}
+	# 	catch {
+	# 		Write-vr_ErrorCode -Code 1403 -Exit $true -Object $PSItem
+	# 	}
+
+	# Create ResourceMgr COM Object
+		Write-Host "    - Create ResourceMgr COM Object"
+
+		try {
+			$Object_MECM_ResourceMgr = New-Object -ComObject "UIResource.UIResourceMgr"
+			Write-Host "        Status: Success"
+		}
+		catch {
+			Write-vr_ErrorCode -Code 1404 -Exit $true -Object $PSItem
+		}
+
+	Write-Host "    - Complete"
+	Write-Host ""
+
+#EndRegion Environment
+#--------------------------------------------------------------------------------------------
+
 
 #--------------------------------------------------------------------------------------------
+# Validation
+#   Error Range: 1500 - 1599
+#--------------------------------------------------------------------------------------------
+#Region Validation
+
+	Write-Host "  Validation"
+
+  # # API Routes
+	# 	Write-Host "    - API Routes"
+
+	# 	try {
+	# 		foreach ($Item in (Get-Variable -Name "Path_AdminService_*")) {
+	# 			Write-Host "        Route: $($Item.Value)"
+
+	# 			$Temp_API_Result = Invoke-RestMethod -Uri $Item.Value -Method Get -ContentType "Application/Json" -UseDefaultCredentials
+	# 			if ($Temp_API_Result) {
+	# 				Write-Host "            Status: Success"
+	# 			}
+	# 			else {
+	# 				Write-Host "            Status: Error"
+	# 				Throw
+	# 			}
+	# 		}
+	# 	}
+	# 	catch {
+	# 		Write-vr_ErrorCode -Code 1501 -Exit $true -Object $PSItem
+	# 	}
+
+	Write-Host "    - Complete"
+	Write-Host ""
+
+#EndRegion Validation
+#--------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------
+# Data Gather
+#   Error Range: 1600 - 1699
+#--------------------------------------------------------------------------------------------
+#Region Data Gather
+
+	Write-Host "  Data Gather"
+
+	# # [WMIClassName]
+  #   Write-Host "    - [WMIClassName]"
+
+  #   try {
+  #     $Dataset_[WMIClassName] = (Invoke-RestMethod -Uri "$($Path_AdminService_WMIRoute)[WMIClassName]" -Method Get -ContentType "Application/Json" -UseDefaultCredentials).value
+  #     Write-Host "        Status: Success"
+  #   }
+  #   catch {
+  #     Write-vr_ErrorCode -Code 1601 -Exit $true -Object $PSItem
+  #   }
+
+	# # [OdataName]
+  #   Write-Host "    - [OdataName]"
+
+  #   try {
+  #     $Dataset_[OdataName] = (Invoke-RestMethod -Uri "$($Path_AdminService_ODataRoute)[OdataName]" -Method Get -ContentType "Application/Json" -UseDefaultCredentials).value
+  #     Write-Host "        Status: Success"
+  #   }
+  #   catch {
+  #     Write-vr_ErrorCode -Code 1602 -Exit $true -Object $PSItem
+  #   }
+
+	# Get Cache Elements
+    Write-Host "    - Get Cache Elements"
+
+    try {
+      $Temp_MECM_CacheElements = $Object_MECM_ResourceMgr.GetCacheInfo().GetCacheElements()
+			$Temp_Counter_Total = $Temp_MECM_CacheElements.Count
+			Write-Host "        Total: $($Temp_Counter_Total)"
+			Write-Host "        Status: Success"
+		}
+		catch {
+			Write-vr_ErrorCode -Code 1603 -Exit $true -Object $PSItem
+		}
+
+	Write-Host "    - Complete"
+	Write-Host ""
+
+#EndRegion Data Gather
+#--------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------
+# Execution
+#   Error Range: 1700 - 1799
+#--------------------------------------------------------------------------------------------
+#Region Execution
+
+	Write-Host "  Execution"
+
+	# Delete Cache Elements
+		Write-Host "    - Delete Cache Elements"
+
+		try {
+			foreach ($Item in $Temp_MECM_CacheElements) {
+        Write-Host "        Content ID: $($Item.ContentId)"
+        Write-Host "        Version: $($Item.ContentVersion)"
+        Write-Host "        Location: $($Item.Location)"
+        Write-Host "        Last Reference Time: $($Item.LastReferenceTime)"
+        Write-Host "        Size (MB): $([math]::Round($Item.ContentSize / 1kb, 2))"
+
+        $MECMObject.GetCacheInfo().DeleteCacheElement([string]$($Item.CacheElementID))
+
+        Write-Host "        Status: Deleted"
+        Write-Host ""
+			}
+		}
+		catch {
+      $Temp_Counter_Error ++
+			Write-vr_ErrorCode -Code 1701 -Exit $false -Object $PSItem
+		}
+
+	# Determine Script Result
+		$Meta_Script_Result = $true,"Success"
+
+	Write-Host "    - Complete"
+	Write-Host ""
+
+#EndRegion Execution
+#--------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------
+# Output
+#   Error Range: 1800 - 1899
+#--------------------------------------------------------------------------------------------
+#Region Output
+
+	Write-Host "  Output"
+
+	# Cache Element Status
+		Write-Host "    - Cache Element Status"
+
+		try {
+			$Temp_MECM_CacheElements_Validate = $Object_MECM_ResourceMgr.GetCacheInfo().GetCacheElements()
+			Write-Host "        Total: $($Temp_MECM_CacheElements_Validate.Count)"
+
+			if ($Temp_MECM_CacheElements_Validate.Count -ne 0) {
+				Write-Host "        Status: Error. Some Elements Not Deleted"
+			}
+			else {
+				Write-Host "        Status: Success"
+			}
+		}
+		catch {
+			Write-vr_ErrorCode -Code 1801 -Exit $false -Object $PSItem
+		}
+
+	Write-Host "    - Complete"
+	Write-Host ""
+
+#EndRegion Output
+#--------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------
+# Cleanup
+#   Error Range: 1900 - 1999
+#--------------------------------------------------------------------------------------------
+#Region Cleanup
+
+	# Write-Host "  Cleanup"
+
+	# # Confirm Cleanup
+	# 	Write-Host "    - Confirm Cleanup"
+
+	# 	do {
+	# 		$Temp_Cleanup_UserInput = Read-Host -Prompt "        Do you want to automatically clean up the unecessary content from this script? [Y]es or [N]o"
+	# 	} until (
+	# 		$Temp_Cleanup_UserInput -in "Y","Yes","N","No"
+	# 	)
+
+	# # [StepName]
+	# 	Write-Host "    - [StepName]"
+
+	# 	try {
+	# 		if ($Temp_Cleanup_UserInput -in "Y", "Yes") {
+
+	# 			Write-Host "        Status: Success"
+	# 		}
+	# 		else {
+	# 			Write-Host "            Status: Skipped"
+	# 		}
+	# 	}
+	# 	catch {
+	# 		Write-vr_ErrorCode -Code 1901 -Exit $true -Object $PSItem
+	# 	}
+
+	# Write-Host "    - Complete"
+	# Write-Host ""
+
+#EndRegion Cleanup
+#--------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------
+# Footer
+#--------------------------------------------------------------------------------------------
+#Region Footer
+
+	# Gather Data
+		$Meta_Script_Complete_DateTime  = Get-Date
+		$Meta_Script_Complete_TimeSpan  = New-TimeSpan -Start $Meta_Script_Start_DateTime -End $Meta_Script_Complete_DateTime
+
+	# Output
+		Write-Host "------------------------------------------------------------------------------"
+		Write-Host "  Script Result: $($Meta_Script_Result[0])"
+		Write-Host "  Script Started: $($Meta_Script_Start_DateTime.ToUniversalTime().ToString(`"yyyy-MM-dd HH:mm:ss`")) (UTC)"
+		Write-Host "  Script Completed: $($Meta_Script_Complete_DateTime.ToUniversalTime().ToString(`"yyyy-MM-dd HH:mm:ss`")) (UTC)"
+		Write-Host "  Total Time: $($Meta_Script_Complete_TimeSpan.Days) days, $($Meta_Script_Complete_TimeSpan.Hours) hours, $($Meta_Script_Complete_TimeSpan.Minutes) minutes, $($Meta_Script_Complete_TimeSpan.Seconds) seconds, $($Meta_Script_Complete_TimeSpan.Milliseconds) milliseconds"
+		Write-Host "------------------------------------------------------------------------------"
+		Write-Host "  End of Script"
+		Write-Host "------------------------------------------------------------------------------"
+
+#EndRegion Footer
+#--------------------------------------------------------------------------------------------
+
+# Stop-Transcript
+Return $Meta_Script_Result[0]
